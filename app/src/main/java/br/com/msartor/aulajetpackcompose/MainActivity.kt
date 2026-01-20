bb4219335180087d8c1da6db8ac1ddfd33dbc78b
marcelosartor <marcelo_sartor@hotmail.com>
2026-01-19 21:17:02 -0300

    lista de dados

--- app/src/main/java/br/com/msartor/aulajetpackcompose/MainActivity.kt
+++ app/src/main/java/br/com/msartor/aulajetpackcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.msartor.aulajetpackcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,22 +9,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,15 +30,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.msartor.aulajetpackcompose.model.Usuario
 
 import br.com.msartor.aulajetpackcompose.ui.theme.AulaJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
+    private val usuarios = listOf(
+        Usuario("Marcelo",52),
+        Usuario("Vanessa",53),
+        Usuario("Joao",25),
+        Usuario("Mickey",3),
+        Usuario("Rose",3),
+        Usuario("Nemo",1),
+        Usuario("Guloso",1),
+        Usuario("Comilão",1),
+        Usuario("Barney",12),
+        Usuario("Ruff",13),
+        Usuario("Rex",0),
+
+        /*
+        Usuario("Marcelo",52),
+        Usuario("Vanessa",53),
+        Usuario("Joao",25),
+        Usuario("Mickey",3),
+        Usuario("Rose",3),
+        Usuario("Nemo",1),
+        Usuario("Guloso",1),
+        Usuario("Comilão",1),
+        Usuario("Barney",12),
+        Usuario("Ruff",13),
+        Usuario("Rex",0),
+
+        Usuario("Marcelo",52),
+        Usuario("Vanessa",53),
+        Usuario("Joao",25),
+        Usuario("Mickey",3),
+        Usuario("Rose",3),
+        Usuario("Nemo",1),
+        Usuario("Guloso",1),
+        Usuario("Comilão",1),
+        Usuario("Barney",12),
+        Usuario("Ruff",13),
+        Usuario("Rex",0),
+        */
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -49,95 +88,175 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-/*---------------------------------------------------------------------------------------
-| INICIO DAS FUNCÇÕES DE COMPOSE                                                        |
-----------------------------------------------------------------------------------------*/
-@Composable
-fun PrimeiroApp(){
+    /*---------------------------------------------------------------------------------------
+    | INICIO DAS FUNCÇÕES DE COMPOSE                                                        |
+    ----------------------------------------------------------------------------------------*/
+    @SuppressLint("Range")
+    @Composable
+    fun PrimeiroApp(){
+        Column(modifier = Modifier
+            //.width(200.dp)
+            //.height(200.dp)
+            .background(Color.Gray)
+            .border(2.dp, color = Color.Red)
+            .padding(0.dp,10.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
 
-    Column(modifier = Modifier
-        //.width(200.dp)
-        //.height(200.dp)
-        .background(Color.Gray)
-        .border(2.dp, color = Color.Red)
-        .padding(30.dp,10.dp)
-        .fillMaxWidth()
-        .fillMaxHeight()
+        ) {
 
-    ) {
-        Text(text = "Marcelo e Vanessa", fontSize = 20.sp)
-        perfilDeUsurio("MS")
-        perfilDeUsurioComImagem()
-        Icon(painter = painterResource(id = R.drawable.ic_add_location_24), contentDescription = "MS")
-        Icon(imageVector = Icons.Default.Lock, contentDescription = "MS")
 
-        Button(onClick = {},
+
+            LazyVerticalGrid(
+                //columns = GridCells.Adaptive(122.dp),
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.padding(10.dp)
             ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(painter = painterResource(id = R.drawable.ic_add_location_24), contentDescription = "MS")
-                Text(text = "Localização(GPS)", fontSize = 8.sp)
+                items(usuarios.size){item->
+                    val nome = usuarios[item].nome
+                    val idade = usuarios[item].idade
+
+                    Row(modifier=Modifier
+                        .padding(2.dp)
+                        .border(2.dp, color = Color.Red,RoundedCornerShape(10.dp, 10.dp, 10.dp, 10.dp))
+                        .padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        perfilDeUsurioColumn(nome.substring(0, 1))
+                        Text(
+                            text = "$nome",
+                            fontSize = 24.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 10.dp)
+
+                        )
+                    }
+                }
             }
+            /*
+
+
+            LazyHorizontalGrid(
+                //columns = GridCells.Adaptive(122.dp),
+                rows = GridCells.Fixed(3),
+                modifier = Modifier.padding(10.dp)
+                    .width(300.dp)
+                    .fillMaxWidth()
+                    .height(200.dp)
+
+            ) {
+                items(usuarios.size){item->
+                    val nome = usuarios[item].nome
+                    val idade = usuarios[item].idade
+                    Row(
+                        modifier = Modifier
+                            .height(64.dp)
+                            .width(300.dp)
+                            .padding(2.dp)
+                            .border(
+                                2.dp,
+                                color = Color.Red,
+                                RoundedCornerShape(10.dp)
+                            )
+                            .padding(horizontal = 10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        perfilDeUsurioColumn(nome.substring(0, 1))
+                        Text(
+                            text = nome,
+                            fontSize = 24.sp,
+                            modifier = Modifier
+                                .padding(start = 10.dp)
+                                .weight(1f)
+                        )
+                    }
+
+                }
+            }
+            */
+
+            /*
+            LazyColumn(modifier = Modifier.padding(16.dp)) {
+                items(usuarios.size){item->
+                    val nome = usuarios[item].nome
+                    val idade = usuarios[item].idade
+
+                    Row(modifier=Modifier
+                        .border(2.dp, color = Color.Red,RoundedCornerShape(10.dp, 10.dp, 10.dp, 10.dp))
+                        .padding(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        perfilDeUsurioColumn(nome.substring(0, 1))
+                        Text(
+                            text = "$nome - $idade",
+                            fontSize = 24.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 10.dp)
+
+                        )
+                    }
+                    Box(Modifier
+                       .padding(10.dp)
+                       .fillMaxWidth()
+                       .height(1.dp)
+                       .background(Color.Blue))
+                }
+            }
+             */
         }
 
     }
 
-    /*Text(
-        text = "Meu primeiro texto 2",
-        color = Color.Black
-    )*/
-    /*Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background( Color.Gray),
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        MyButton("Meu texto") { }
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background( Color.Red),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        )
-        {
+    @Preview
+    @Composable
+    fun PrimeiroAppPreview(){
+        PrimeiroApp()
+    }
 
-            Text(text = "col 1", color = Color.Black, fontSize = 24.sp )
-            Text(text = "col 2", color = Color.Black, fontSize = 24.sp )
-            Text(text = "col 3", color = Color.Black, fontSize = 24.sp )
-        }
-
-        Text(text = "linha 1", color = Color.Black, fontSize = 24.sp )
-        Text(text = "Linha 2", color = Color.Black, fontSize = 24.sp )
-        Text(text = "Linha 3", color = Color.Black, fontSize = 24.sp )
-
-
-        *//*Button(onClick = {}) {
-            Text(
-                text = "Meu primeiro texto 2",
-                color = Color.Black
-            )
-        }*//*
-    }*/
 
 }
+
+
 @Composable
 fun perfilDeUsurio(text: String){
     Column(
         modifier = Modifier
             .width(100.dp)
             .height(100.dp)
-            .border(2.dp, color = Color.Red, RoundedCornerShape(topStart = 30.dp, bottomEnd = 30.dp))
-            //.clip(shape = CircleShape)
-            .clip(shape = RoundedCornerShape(topStart = 30.dp, bottomEnd = 30.dp))
+            .border(2.dp, color = Color.Red,  CircleShape)
+            .clip(shape = CircleShape)
+            //.clip(shape = RoundedCornerShape(topStart = 30.dp, bottomEnd = 30.dp))
             //.clip(CutCornerShape(10.dp))
             .background(Color.Green),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "MS", fontSize = 32.sp)
+        Text(text = text, fontSize = 32.sp)
+    }
+}
+
+@Composable
+fun perfilDeUsurioColumn(text: String ){
+    Box(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .border(2.dp, Color.Red, CircleShape)
+            .clip(CircleShape)
+            .background(Color.Green),
+
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
@@ -168,11 +287,6 @@ fun perfilDeUsurioComImagem(){
     }
 }
 
-@Preview
-@Composable
-fun PrimeiroAppPreview(){
-    PrimeiroApp()
-}
 
 
 
---
+++ app/src/main/java/br/com/msartor/aulajetpackcompose/model/Usuario.kt
@@ -0,0 +1,7 @@
+package br.com.msartor.aulajetpackcompose.model
+
+data class Usuario(
+    val nome: String="",
+    val idade: Int=0,
+) {
+}
